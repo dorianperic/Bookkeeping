b5754bd2a057bbb02c25f5a528d94fd85a025cec
dorianperic <dperic00@fesb.hr>
2022-06-24 00:36:55 +0200

    Implemented create & update pages for BO

--- server.js
+++ server.js
@@ -31,5 +31,14 @@ app.get('/', (req,res)=>{
 app.get('/books', (req,res)=>{
     res.render('gallery');
 })
+
+app.get('/add-book', (req,res)=>{
+    res.render('backofficeaddbook');
+})
+
+app.get('/update-book', (req,res)=>{
+    res.render('backofficeupdatebook');
+})
+
 app.listen(PORT, ()=> {console.log(`serv run on http://localhost:${PORT}`)});
 
